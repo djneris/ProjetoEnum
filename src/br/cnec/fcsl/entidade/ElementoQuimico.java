943f77b06662bf71ffd93d1dcb2f86775c953447
Danilo Neri <oliveira.daniloneri@gmail.com>
2017-05-17 23:45:35 -0300

    Adicionado mais elementos

--- src/br/cnec/fcsl/entidade/ElementoQuimico.java
+++ src/br/cnec/fcsl/entidade/ElementoQuimico.java
@@ -4,17 +4,24 @@ public enum ElementoQuimico {
 	H ("H", "Hidrogenio", 1, 1.00794, Familia.NAO_METAIS),
 	HE ("He", "Hélio", 2, 4.002602, Familia.GASES_NOBRES),
 	LI ("Li", "Lítio", 3, 6.941, Familia.METAIS_ALCALINOS),
-	BE ("Be", "Berílio", 4, 9.012182, Familia.METAIS_ALCALINOS);
+	BE ("Be", "Berílio", 4, 9.012182, Familia.METAIS_ALCALINOS),
+	B ("B", "Boro", 5, 10.811, Familia.SEMIMETAIS),
+	C ("C", "Carbono", 6, 12.0107, Familia.NAO_METAIS),
+	N ("N", "Nitrogênio", 7, 14.0067, Familia.NAO_METAIS),
+	O ("O" ,"Oxigênio", 8, 15.9994, Familia.NAO_METAIS),
+	F ("F", "Flúor", 9, 18.9984032, Familia.HALOGENIOS),
+	NE ("Ne", "Neônio", 10, 20.1797, Familia.GASES_NOBRES),
+	TM ("Tm", "Túlio", 69, 168.93421, Familia.METAIS_LANTANIDEOS);
 	
 	
-	private ElementoQuimico(String simbolo, String nome, Integer numero, double massa, Familia familia) {
+	private ElementoQuimico(String simbolo, String nome, Integer numero, Double massa, Familia familia) {
 		this.simbolo = simbolo;
 		this.nome = nome;
 		this.numero = numero;
 		this.massa = massa;
 		this.familia = familia;
 	}
-	
+
 	private String simbolo, nome;
 	private Integer numero;
 	private Double massa;
@@ -44,4 +51,5 @@ public enum ElementoQuimico {
 		return nome;
 		
 	}
+
 }
